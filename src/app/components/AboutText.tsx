1e67e1377ee028d8b7632abcbeeee69b524b3b05
Matt Thomson <mthomson.lee@gmail.com>
2018-04-06 17:38:09 -0400

    Justify text

--- src/app/components/AboutText.tsx
+++ src/app/components/AboutText.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
 
-export const AboutText = () => 
-  
+export const AboutText = () =>
+
   <section>
     <h1>Stay awhile and explore.</h1>
-    <p>I'm a natural light photographer with a passion for capturing the most organic emotions. The kind of emotion that cannot be posed but simply felt. I love to produce work that tells a story and ignites a feeling within. Whether it be from an in-home session with the love(s) of your life, a lifestyle session portraying a glimpse into your life or a celebration you want to cherish forever. I desire to connect with you and document the connections that inspire the love and passion in your life in a form of art.</p>
+    <p className="justify">I'm a natural light photographer with a passion for capturing the most organic emotions. The kind of emotion that cannot be posed but simply felt. I love to produce work that tells a story and ignites a feeling within. Whether it be from an in-home session with the love(s) of your life, a lifestyle session portraying a glimpse into your life or a celebration you want to cherish forever. I desire to connect with you and document the connections that inspire the love and passion in your life in a form of art.</p>
   </section>
-  
